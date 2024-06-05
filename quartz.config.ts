cfccb97e490e121a03e38a5928474a15f5ab1b57
ReinerBalschun <reinermbalschun@gmail.com>
2024-06-05 19:56:43 +0200

    Quartz sync: Jun 5, 2024, 7:56 PM

--- quartz.config.ts
+++ quartz.config.ts
@@ -28,7 +28,7 @@ const config: QuartzConfig = {
       },
       colors: {
         lightMode: {
-          light: "#ffd0b3",
+          light: "#fff0e6",
           lightgray: "#e5e5e5",
           gray: "#b8b8b8",
           darkgray: "#4e4e4e",
