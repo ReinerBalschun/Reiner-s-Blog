816fb0f6f964cec14f98901d6c7ed002ca777206
Reiner Balschun <reinermbalschun@gmail.com>
2024-06-13 10:38:21 +0200

    header font test "honk" test 2

--- quartz.config.ts
+++ quartz.config.ts
@@ -22,7 +22,7 @@ const config: QuartzConfig = {
       fontOrigin: "googleFonts",
       cdnCaching: true,
       typography: {
-        header: "Nabla",
+        header: "Honk",
         body: "Source Sans Pro",
         code: "IBM Plex Mono",
       },
